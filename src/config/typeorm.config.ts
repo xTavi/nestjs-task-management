d42cf9872f469edd3757fcd5a2600b5e53be9637
xTavi <tavitavi62@gmail.com>
2021-05-25 22:18:18 +0300

    Added docker and new connection strings

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,9 +3,9 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
-  port: 5432,
+  port: 5431,
   username: 'postgres',
-  password: 'postgres',
+  password: 'pass1234',
   database: 'taskmanagement',
   autoLoadEntities: true,
   synchronize: true,
