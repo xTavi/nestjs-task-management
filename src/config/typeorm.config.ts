a10821590f0fb56255cb3805dd1288f299d8548f
xTavi <tavitavi62@gmail.com>
2021-05-27 23:42:28 +0300

    Added new port for db conflicts :D

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
-  port: 5432,
+  port: 5555,
   username: 'postgres',
   password: 'postgres',
   database: 'task-management',
