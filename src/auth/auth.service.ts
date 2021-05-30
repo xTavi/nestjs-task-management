1554d4ecc9495120a39036597afe1223ddfe9517
xTavi <tavitavi62@gmail.com>
2021-05-31 00:39:33 +0300

    Created sign up method + create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './auth-credentials.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -7,4 +8,8 @@ export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
   ) {}
+
+  public signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialsDto);
+  }
 }
