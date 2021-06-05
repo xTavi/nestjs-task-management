ecea58cdad67a8cacfab15dd26ebb4294a5eedfc
xTavi <tavitavi62@gmail.com>
2021-06-06 01:52:57 +0300

    Added validation schema for config values

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigValidationSchema } from './config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: ConfigValidationSchema,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
