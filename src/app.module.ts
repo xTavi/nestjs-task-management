36ee99a8fb5b7bf06401cca4f2cc97378d100ff9
xTavi <tavitavi62@gmail.com>
2021-06-06 01:32:13 +0300

    Deleted typeorm.config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
