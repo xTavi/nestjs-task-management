4fb4c11e4a02d22d7b8d4f14b0d91607721fb4b2
xTavi <tavitavi62@gmail.com>
2021-06-07 20:13:26 +0300

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { TransformInterceptor } from './transform.interceptor';
 async function bootstrap() {
   const logger = new Logger('', true);
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   const port = 9999;
