4426fb825fcd4c7d5f787d232e60f160ee428c45
xTavi <tavitavi62@gmail.com>
2021-06-11 21:11:07 +0300

    Created unit test for get Tasks from user service

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,14 +1,42 @@
 import { Test } from '@nestjs/testing';
+import { User } from 'src/auth/user.entity';
 import { TaskRepository } from './task.repository';
 import { TasksService } from './tasks.service';
 
+const mockTaskRepository = () => ({
+  getTasks: jest.fn(),
+});
+
+const mockUser: User = {
+  username: 'Tavi',
+  id: '123',
+  password: 'securePassword',
+  tasks: [],
+};
+
 describe('Tasks service', () => {
   let tasksService: TasksService;
-  let taskRepository: TaskRepository;
+  let taskRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [TaskRepository],
+      providers: [
+        TasksService,
+        { provide: TaskRepository, useFactory: mockTaskRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get<TasksService>(TasksService);
+    taskRepository = module.get<TaskRepository>(TaskRepository);
+  });
+
+  describe('get Tasks', () => {
+    it('calls TasksRepository.getTasks and returns the result', async () => {
+      expect(taskRepository.getTasks).not.toHaveBeenCalled();
+      taskRepository.getTasks.mockResolvedValue('Some value');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(taskRepository.getTasks).toHaveBeenCalled();
+      expect(result).toEqual('Some value');
     });
   });
 });
